b8ace546c777fa119de3bc52e540aa73fb7fb6f4
Reshetnikov_a <ar@smartlombard.ru>
2025-10-05 16:47:36 +0300

    unit #1

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <h3>{{ info }}</h3>
+    <p>{{ some_info }}</p>
+    <button
+        type="button"
+        @click="userData('Очищено!')">
+        Отправить
+    </button>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                info: 'Title!',
+                some_info: 'Текст'
+            }
+        },
+        methods: {
+            userData(world = 'Default') {
+                this.some_info = world;
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    p {
+        color: gray;
+    }
+</style>
