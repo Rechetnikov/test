38bec5d63958a64be5d429dcf092e2d0e83c984b
Reshetnikov_a <ar@smartlombard.ru>
2025-10-05 17:48:48 +0300

    unit #3

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-    <input type="text" @input="insertData($event.target.value)" placeholder="Логин"><br />
-    <input type="password" placeholder="Пароль" /><br />
-    <input type="emil" placeholder="E-mail" />
+    <p><input type="text" v-model="user_name" placeholder="Логин"></p>
+    <p><input type="password" v-model="user_pass" placeholder="Пароль" /></p>
+    <p><input type="email" v-model="user_email" placeholder="E-mail" /></p>
 
-    <p>{{user_name}}</p>
+    <p className="error">{{ error }}</p>
+
+    <p><button @click="sendData()">Отправить</button> <button @click="clearUsers()">Очистить</button></p>
+
+    <div v-if="users.length == 0">Нет пользователей</div>
+
+    <div v-for="(item, index) in users" :key="index">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.email }} - <b>{{item.pass}}</b></p>
+    </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
+                users: [],
                 user_name: '',
                 user_pass: '',
-                user_emil: ''
+                user_email: '',
+                error: ''
             }
         },
         methods: {
-            insertData(value) {
-                this.user_name = value
+            sendData() {
+                if (this.user_name == '') {
+                    this.error = "Поле \"Логин\" не должно быть пустым!";
+
+                    return;
+                } else if (this.user_pass == '') {
+                    this.error = "Поле \"Пароль\" не должно быть пустым!";
+
+                    return;
+                } else if (this.user_email == '') {
+                    this.error = "Поле \"E-mail\" не должно быть пустым!";
+
+                    return;
+                }
+
+                this.error = '';
+
+                this.users.push({
+                    name: this.user_name,
+                    pass: this.user_pass,
+                    email: this.user_email
+                });
+            },
+            clearUsers() {
+                this.users = [];
+                this.error = '';
             }
         }
     };
 </script>
 
 <style scoped>
-
+    p, div, button {
+        font-family: Verdana;
+    }
+    .error {
+        color: red;
+    }
 </style>
